65757f0603374799f562b14ef2c8c3d9cf4f1400
DongHoonYu96 <dbehdgns71@naver.com>
2023-12-06 01:49:33 +0900

    docs : 캐릭터 움직임 설정 주석추가

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -1,18 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+// 프로젝트 설정의 Description 페이지에 저작권 고지를 작성합니다.
 
 #include "Character/AuraCharacter.h"
-
 #include "GameFramework/CharacterMovementComponent.h"
 
 AAuraCharacter::AAuraCharacter()
 {
-	GetCharacterMovement()->bOrientRotationToMovement=true;
-	GetCharacterMovement()->RotationRate=FRotator(0.f,400.f,0.f);
-	GetCharacterMovement()->bConstrainToPlane=true;
-	GetCharacterMovement()->bSnapToPlaneAtStart=true;
+	// 캐릭터의 움직임에 관련된 속성을 설정합니다.
 
-	bUseControllerRotationPitch=false;
-	bUseControllerRotationRoll=false;
-	bUseControllerRotationYaw=false;
+	// 캐릭터가 움직임에 따라 회전하여 이동 방향을 바라보도록 설정합니다.
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	// 캐릭터가 회전하는 비율을 설정합니다 (Yaw 축).
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+
+	// 캐릭터의 움직임을 평면에 제한하여 Z축으로의 움직임을 막습니다.
+	GetCharacterMovement()->bConstrainToPlane = true;
+
+	// 시작 시 캐릭터를 이동 평면에 정렬합니다.
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 컨트롤러 기반 회전을 비활성화합니다 (Pitch, Roll, Yaw 축).
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
 }
