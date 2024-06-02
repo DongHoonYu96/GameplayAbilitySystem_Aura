0064748b3163e777b8ff2848a76d004476ebbb69
DongHoonYu96 <dbehdgns71@naver.com>
2024-06-02 18:55:40 +0900

    feat : 불에닿으면 피감소구현 / 벗어나면 멈춤

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -21,18 +21,59 @@ void AAuraEffectActor::BeginPlay()
 
 }
 
-void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+
+
+
+//정책에 따라 분기
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
 {
-	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
-	if(nullptr==TargetASC) return; //target이 ASC가 없는경우
+	if(InstantEffectApplicationPolicy==EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if(DurationEffectApplicationPolicy==EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if(InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+}
 
-	check(GameplayEffectClass); //효과가 있는지 검사
-	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext(); //게임플레이 효과에 대한 컨텍스트 정보를 저장하는 데 사용되는 구조체생성
-	EffectContextHandle.AddSourceObject(this); //현재 개체(효과를 호출하는 액터나 구성 요소 등)를 효과 컨텍스트에 추가합니다. 이는 적용 시 효과의 소스를 식별하는 데 도움이 됩니다.
-	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass,1.f, EffectContextHandle);
-	// 적용될 게임플레이 효과에 대한 사양이 포함된 'FGameplayEffectSpecHandle'을 생성
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); // 대상의 AbilitySystemComponent에 효과 사양(효과의 세부 사항)을 적용
-	//이 프로세스를 통해 게임플레이 효과가 게임의 능력 시스템 내에서 적절하게 맥락화되고, 지정되고, 적용됩니다.
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if(InstantEffectApplicationPolicy==EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if(DurationEffectApplicationPolicy==EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if(InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+	if(InfiniteEffectRemovalPolicy==EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		if(!IsValid(TargetASC)) return;
 
-	
+		TArray<FActiveGameplayEffectHandle> HandlesToRemove;
+		for(TTuple<FActiveGameplayEffectHandle, UAbilitySystemComponent*> HandlePair : ActiveEffectHandles)
+		{
+			if(TargetASC == HandlePair.Value)
+			{
+				//1. 효과제거
+				TargetASC->RemoveActiveGameplayEffect(HandlePair.Key,1);
+				HandlesToRemove.Add(HandlePair.Key); // 2. 제거할 핸들에 추가
+			}
+		}
+		//2. 핸들을 맵에서 제거
+		for(FActiveGameplayEffectHandle& Handle : HandlesToRemove)
+		{
+			ActiveEffectHandles.FindAndRemoveChecked(Handle);
+		}
+	}
 }
