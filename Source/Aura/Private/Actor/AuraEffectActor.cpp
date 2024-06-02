09a3ffa2c7f802ab870ad3369d11c2e040d9da73
DongHoonYu96 <dbehdgns71@naver.com>
2024-06-02 14:41:27 +0900

    feat : 지속 이펙트 구현
    크리스탈먹으면 최대체력 100증가 2초간

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -21,9 +21,9 @@ void AAuraEffectActor::BeginPlay()
 
 }
 
-void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 	if(nullptr==TargetASC) return; //target이 ASC가 없는경우
 
 	check(GameplayEffectClass); //효과가 있는지 검사
@@ -33,4 +33,6 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplay
 	// 적용될 게임플레이 효과에 대한 사양이 포함된 'FGameplayEffectSpecHandle'을 생성
 	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); // 대상의 AbilitySystemComponent에 효과 사양(효과의 세부 사항)을 적용
 	//이 프로세스를 통해 게임플레이 효과가 게임의 능력 시스템 내에서 적절하게 맥락화되고, 지정되고, 적용됩니다.
+
+	
 }
