558542c850094c17bdce9825db82f19aafde169a
DongHoonYu96 <dbehdgns71@naver.com>
2024-06-19 00:47:47 +0900

    feat : 캐릭터 종류 설계, 레벨별 속성값 설정

---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType) 
+enum class ECharacterClass : uint8 //캐릭구분용
+{
+	Elementalist, //0
+	Warrior, //1
+	Ranger //2
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category="Class Defaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes; //그 캐릭터만의 고유속성
+};
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Character Class Defaults") //<캐릭터, 정보> 맵
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+
+	//공통속성
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass); //@Getter
+	
+};
